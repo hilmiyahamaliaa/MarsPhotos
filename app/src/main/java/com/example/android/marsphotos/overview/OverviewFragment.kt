433581bdbf2def1bca73d8d5f73d242ef4b13f01
hilmiyahamaliaa <hilmiyahamaliaa@gmail.com>
2022-05-22 12:31:38 +0800

    initial commit

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -24,17 +24,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.marsphotos.databinding.FragmentOverviewBinding
 
-/**
- * This fragment shows the the status of the Mars photos web services transaction.
- */
 class OverviewFragment : Fragment() {
 
     private val viewModel: OverviewViewModel by viewModels()
 
-    /**
-     * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
-     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
-     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,6 +39,8 @@ class OverviewFragment : Fragment() {
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         return binding.root
     }
